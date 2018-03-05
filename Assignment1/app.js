79117e91d13e184a30d4d63f718c5561e87f23b9
Bob <coursera@parkridge.on.ca>
2018-03-04 20:17:15 -0500

    Add Assignment2

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -1,33 +1,47 @@
 (function() {
 'use strict'
 
+// Declare the Angular module
+// and the LunchCheck controller
 angular.module('LunchCheck', [] )
 .controller('LunchCheckController', LunchCheckController );
 
+// Dependancy Injection
 LunchCheckController.$inject = ['$scope'];
+
 function LunchCheckController($scope) {
+	// setup the default values
 	$scope.listOfDishes = "";
 	$scope.message = "";
 	$scope.messageStyle = "";
+
+	// define the function that will check the
+	// number of lunch items
 	$scope.checkTooMuch = function() {
 		$scope.messageStyle = {color: 'green', border: '2px solid green'};
+
+		// Check for an empty field first
+		// if the field is empty prompt the user to enter some information
 		if ( $scope.listOfDishes.length == 0 ) {
 			$scope.message = "Please enter data first.";
 			$scope.messageStyle = {color: 'red', border: '2px solid Red'};
 			return;
 		}
+
+		// Split the number of dishes into an array so they can be counted
+		// and checked for zero length
 		var dishes = $scope.listOfDishes.split(",");
 		var numberOfDishes = dishes.length;
 
-		console.log( dishes, numberOfDishes );
-
+		// scan the list looking for any empty strings
+		// where the user has entered a,,b
 		for ( var d in dishes ) {
-			console.log( dishes[d], dishes[d].length );
 			if ( dishes[d].length == 0 ) {
 				numberOfDishes--;
 			}
 		}
 
+		// determine if the number of items is OK or not.
 		if ( numberOfDishes < 4 ) {
 			$scope.message = "Enjoy!";
 		}
---
+++ Assignment2/app.js
@@ -0,0 +1,43 @@
+(function() {
+'use strict'
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController )
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService );
+
+ToBuyController.inject = ['$scope', 'ShoppingListCheckOffService'];
+function ToBuyController ($scope, ShoppingListCheckOffService) {
+	$scope.toBuyList = ShoppingListCheckOffService.getToBuyList();
+	
+	$scope.boughtItem = function( idx ) {
+		ShoppingListCheckOffService.boughtItem( idx );
+	}
+}
+
+AlreadyBoughtController.inject = ['$scope', 'ShoppingListCheckOffService'];
+function AlreadyBoughtController ($scope, ShoppingListCheckOffService) {
+	$scope.boughtList = ShoppingListCheckOffService.getBoughtList();
+}
+
+function ShoppingListCheckOffService () {
+	var toBuy = ['1 Litre Milk', '1 Loaf of Bread', '2 Dozen Eggs', '1 Box Icecream Bars' ];
+	var bought = [];
+
+	this.boughtItem = function ( idx ) {
+		console.log( 'Bought item', idx );
+		var item = toBuy[idx];
+		toBuy.splice( idx, 1 );
+		bought.push( item );
+	}
+
+	this.getToBuyList = function () {
+		return ( toBuy );
+	}
+
+	this.getBoughtList = function () {
+		return ( bought );
+	}
+}
+
+})();
